3a8975b1eb49b220fe3181630a49c6b1202441e8
Kishkin Vitalii <dimpla30@gmail.com>
2023-02-16 18:39:08 +0300

    add CompactCard component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import { AnimateSharedLayout } from "framer-motion";
+import "react-circular-progressbar/dist/styles.css";
+import "./Card.css";
+
+const Card = props => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <AnimateSharedLayout>
+      {expanded ? "ExpandedCard" : <CompactCard param={props} />}
+    </AnimateSharedLayout>
+  );
+};
+
+// CompactCard
+function CompactCard({ param }) {
+  const Png = param.png;
+  return (
+    <div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+    >
+      <div className="radialBar">
+        <CircularProgressbar
+          value={param.barValue}
+          text={`${param.barValue}%`}
+        />
+        <span>{param.title}</span>
+      </div>
+      <div className="detail">
+        <Png />
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
